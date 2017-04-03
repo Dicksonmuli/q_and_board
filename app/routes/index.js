2a9da217ee09cb8397be1428e6b8b80aa603155f
dicksonmuli <dicksonmully6@gmail.com>
2017-04-03 15:29:27 +0300

    answer details

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,14 +6,14 @@ export default Ember.Route.extend({
 	model(){
 		return Ember.RSVP.hash({
 			questions: this.store.findAll('question'),
-			answers: this.store.findAll('answer'),
-});
-},
-actions: {
-	saveQtn(params){
-		var newQuestion =  this.store.createRecord('question', params);
-		newQuestion.save();
-		this.transitionTo('index');
+			answers: this.store.findAll('answer')
+		});
+	},
+	actions: {
+		saveQtn(params){
+			var newQuestion =  this.store.createRecord('question', params);
+			newQuestion.save();
+			this.transitionTo('index');
+		}
 	}
-}
 });
