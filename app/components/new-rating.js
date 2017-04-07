f3b90e9fda7c9f4ae4c94fb178e3028ab37459fd
dicksonmuli <dicksonmully6@gmail.com>
2017-04-07 15:32:55 +0300

    rating- rating form and saving new form

--- app/components/new-rating.js
+++ app/components/new-rating.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+	ratingForm: false,
+//display form and save answer actions
+	actions: {
+		newRating(){
+			this.set('ratingForm', true);
+
+		},
+		saveRating(){
+			var params = {
+				rating: parseInt(this.get('rating'))
+			};
+			this.set('ratingForm', false);
+			this.sendAction('saveRating', params);
+		}
+	}
 });
