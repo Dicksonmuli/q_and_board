89f6a4dcc515a9bdce45d8ca970fbb092a7a603b
dicksonmuli <dicksonmully6@gmail.com>
2017-04-07 16:54:52 +0300

    rating controller added

--- app/controllers/answer.js
+++ app/controllers/answer.js
@@ -1,4 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+	actions: {
+        voteUp: function () {
+            this.set('votes', this.get('votes') + 1);
+        },
+        voteDown: function () {
+            console.log("voting down");
+            this.set('votes', this.get('votes') - 1);
+        }
+    }
+
 });
